b8e3f470cc22f657184528e06cbcb89dddb96ac8
dibovdmitry <mega.dmitry2002@mail.ru>
2022-05-18 22:21:39 +0300

    add changes for Individual2.py

--- Individual2.py
+++ Individual2.py
@@ -1,59 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
 import argparse
-import sys
 import pathlib
 import colorama
-import os
 from colorama import Fore
-import magic
 
 
 def tree(directory):
-    print(Fore.RED + f'> {directory}')
-    for path1 in sorted(directory.rglob('*')):
-        depth = len(path1.relative_to(directory).parts)
-        spacer = '  ' * depth
-        if os.path.isdir(path1):
-            print(Fore.RED + f'{spacer} > {path1.name}')
-        else:
-            file = str(path1)
-            type_file = magic.from_file(file, mime=True)
-            if (type_file.split('/')[0]) == "audio":
-                print(Fore.BLUE + f'{spacer} < {path1.name}')
-            elif (type_file.split('/')[0]) == "video":
-                print(Fore.CYAN + f'{spacer} < {path1.name}')
-            elif (type_file.split('/')[0]) == "image":
-                print(Fore.GREEN + f'{spacer} < {path1.name}')
-            elif (type_file.split('/')[0]) == "text":
-                print(Fore.YELLOW + f'{spacer} < {path1.name}')
-            elif (type_file.split('/')[0]) == "application":
-                print(Fore.MAGENTA + f'{spacer} < {path1.name}')
-            else:
-                print(Fore.BLACK + f'{spacer} < {path1.name}')
-
-
-def tree_find(directory, suffix_select):
-    root = str(directory)
-    answer = []
-    spacer = "  "
-
-    for current, directories, files in os.walk(root):
-        for f in files:
-            if f.endswith(suffix_select):
-                d = current
-                if d not in answer:
-                    answer.append(d)
-                    print(Fore.RED + f'> {d}')
-                print(Fore.BLUE + f'{spacer} < {f}')
+    print(Fore.LIGHTGREEN_EX + f'>>> {directory}')
+    for path in sorted(directory.rglob('*')):
+        depth = len(path.relative_to(directory).parts)
+        spacer = ' ' * depth
+        print(Fore.LIGHTYELLOW_EX + f'{spacer} >> {path.name}')
+        for new_path in sorted(directory.joinpath(path).glob('*')):
+            depth = len(new_path.relative_to(directory.joinpath(path)).parts)
+            spacer = '\t' * depth
+            print(Fore.MAGENTA + f'{spacer} > {new_path.name}')
 
 
 def main(command_line=None):
-
     colorama.init()
     current = pathlib.Path.cwd()
-
     file_parser = argparse.ArgumentParser(add_help=False)
 
     parser = argparse.ArgumentParser("tree")
@@ -61,49 +30,62 @@ def main(command_line=None):
         "--version",
         action="version",
         help="The main parser",
-        version="%(prog)s 1.0"
+        version="%(prog)s 0.1.0"
     )
 
     subparsers = parser.add_subparsers(dest="command")
 
-    # Субпарсер для выбора пути
-    select_dir = subparsers.add_parser(
-        "select",
+    create = subparsers.add_parser(
+        "mkdir",
         parents=[file_parser]
     )
-    select_dir.add_argument(
-        "path",
+    create.add_argument(
+        "filename",
         action="store"
     )
 
-    # Субпарсер для поиска файлов
-    find_file = subparsers.add_parser(
-        "find",
+    create = subparsers.add_parser(
+        "rmdir",
         parents=[file_parser]
     )
-    find_file.add_argument(
-        "suffix",
+    create.add_argument(
+        "filename",
         action="store"
     )
-    find_file.add_argument(
-        "path",
-        nargs='?',
-        default=current,
-    )
 
+    create = subparsers.add_parser(
+        "touch",
+        parents=[file_parser]
+    )
+    create.add_argument(
+        "filename",
+        action="store"
+    )
+    create = subparsers.add_parser(
+        "rm",
+        parents=[file_parser]
+    )
+    create.add_argument(
+        "filename",
+        action="store"
+    )
     args = parser.parse_args(command_line)
-
-    if args.command == 'select':
-        directory = pathlib.Path(args.path)
-        if directory.exists():
-            tree(directory)
-        else:
-            print(Fore.RED + "Путь не существует", file=sys.stderr)
-            exit(1)
-    elif args.command == 'find':
-        suffix = args.suffix
-        directory = pathlib.Path(args.path)
-        tree_find(directory, suffix)
+    if args.command == 'mkdir':
+        directory_path = current / args.filename
+        directory_path.mkdir()
+        tree(current)
+    elif args.command == "rmdir":
+        directory_path = current / args.filename
+        directory_path.rmdir()
+        tree(current)
+    elif args.command == "touch":
+        directory_path = current / args.filename
+        directory_path.touch()
+        tree(current)
+    elif args.command == "rm":
+        directory_path = current / args.filename
+        directory_path.unlink()
+        tree(current)
     else:
         tree(current)
 
